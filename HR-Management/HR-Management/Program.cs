3474efc3546cdd2622c4fb215576c762b5156e82
nandashalihin <nandashalihin0@gmail.com>
2023-09-19 23:00:04 +0700

    HR-Management database link dasar

---
+++ HR-Management/HR-Management/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BasicConnectivity;
+
+public class Program
+{
+    private static readonly string connectionString =
+        "Data Source=LAPTOP-F1TDVNCH;Integrated Security=True;Database=human_resources;Connect Timeout=30;";
+
+    private static void Main()
+    {
+        //GetAllRegions();
+        DeleteRegion(10);
+    }
+
+    // GET ALL: Region
+    public static void GetAllRegions()
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM tbl_regions";
+
+        try
+        {
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+                while (reader.Read())
+                {
+                    Console.WriteLine("Id: " + reader.GetInt32(0));
+                    Console.WriteLine("Name: " + reader.GetString(1));
+                }
+            else
+                Console.WriteLine("No rows found.");
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // GET BY ID: Region
+    public static void GetRegionById(int id) { }
+
+    // INSERT: Region
+    public static void InsertRegion(string name)
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO tbl_regions VALUES (@name);";
+
+        try
+        {
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                switch (result)
+                {
+                    case >= 1:
+                        Console.WriteLine("Insert Success");
+                        break;
+                    default:
+                        Console.WriteLine("Insert Failed");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+   
+    // UPDATE: Region
+    public static void UpdateRegion(int id, string name)
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "UPDATE tbl_regions SET Name = @name WHERE Id = @id;";
+
+        try
+        {
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.Int;
+            command.Parameters.Add(pId);
+
+            var pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.Value = name;
+            pName.SqlDbType = SqlDbType.VarChar;
+            command.Parameters.Add(pName);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                if (result > 0)
+                {
+                    Console.WriteLine("Update Success");
+                }
+                else
+                {
+                    Console.WriteLine("Update Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    // DELETE: Region
+    public static void DeleteRegion(int id)
+    {
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = "DELETE FROM tbl_regions WHERE Id = @id;";
+
+        try
+        {
+            var pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.Value = id;
+            pId.SqlDbType = SqlDbType.Int;
+            command.Parameters.Add(pId);
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                if (result > 0)
+                {
+                    Console.WriteLine("Delete Success");
+                }
+                else
+                {
+                    Console.WriteLine("Delete Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Error Transaction: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+
+}
